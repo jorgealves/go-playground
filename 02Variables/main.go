ccc46bcbb6f322f0b58c39fa1eaa0eb22cceec03
Jorge André Alves <jorgeandrealves@gmail.com>
2018-03-23 00:06:37 +0000

    :gear: added 3 main Golang concepts, Main func, variable and functions

---
+++ 02Variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	variable := "asd"
+	fmt.Printf("%s", variable)
+	fmt.Println()
+	fmt.Printf("%q", variable)
+	fmt.Println()
+	fmt.Printf("%x", variable)
+	fmt.Println()
+	fmt.Printf("%X", variable)
+	fmt.Println()
+}
---
+++ 03Functions/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func printName(name string) string {
+	return name
+}
+
+func main() {
+	fmt.Println(printName("Hello World with function"))
+}
